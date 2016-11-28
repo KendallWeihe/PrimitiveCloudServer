d76acd43128a24b0980a7cccbe9d7d64c2732241
Kendall Weihe <kendallweihe@gmail.com>
2016-11-28 15:54:12 -0500

    added server pseudocode

--- server.cpp
+++ server.cpp
@@ -5,4 +5,32 @@ using namespace std;
 
 int main(int argc, char* argv[]){
 
+  // TODO:
+    // check for correct arguments
+    // Open_listenfd()
+    // while(1)
+    //   Accept()
+    //   Gethostbyaddr()
+    //   Rio_readinitb()
+    //   Rio_readnb()
+    //   process data:
+    //     determine type of request
+    //     if put:
+    //       save to file
+    //       Rio_writen() --> response?
+    //     else if get:
+    //       open file
+    //       read file
+    //       Rio_writen() --> file unfo
+    //     else if del:
+    //       execve("rm file.data")
+    //       Rio_writen() --> response?
+    //     else if list:
+    //       execve("ls")
+    //       capture output
+    //       Rio_writen() --> output from "ls"
+    //     else:
+    //       Rio_writen() --> command not recognized
+    //   Close()
+
 }
